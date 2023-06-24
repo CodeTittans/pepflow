51d2d7fff6debe25ae6ee69e6248d8dee48ca353
oabdin <osama.abdin@mail.utoronto.ca>
2023-06-24 00:38:00 -0400

    PepFlow codebase

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='pepflow',
+   version='1.0',
+   description='A module for all-atom peptide conformation sampling',
+   author='Osama Abdin',
+   author_email='osama.abdin@kimlab.org',
+   packages=  find_packages(),
+   include_package_data=True,
+
+)
